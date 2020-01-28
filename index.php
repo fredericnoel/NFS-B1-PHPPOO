a32e1db426a39f843371c3b9a89458b4d4a07df8
Frédéric NOËL <contact@fredericnoel.com>
2020-01-28 16:39:50 +0100

    Ajout fuseau horaire

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
+date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
 
-$chat = new Mammifere();
-var_dump($chat);
-
-
-$toto = new Coupe("Peugeot", "504", "vert", [2000, 1000, 1000], 1200);
-var_dump($toto);
-
-$voiture1 = new Voiture("Lada", "Niva", "blanc", [2000, 1500, 800], 900);
-$voiture2 = new Voiture("Nissan", "Cube", "violet", [2500, 1800, 1200], 1200);
-
-echo $voiture1->getMarque();
-
-
-$voiture1->vitesse = 25;
-$voiture2->vitesse = 30;
-
-//echo $voiture1->calculerEnergieCinetique() . "<br />";
-//$voiture1->vitesse = 20;
-//echo $voiture1->calculerEnergieCinetique();
