0eb177b77a9c0ca9fa1d62af23645f58518b71d6
Frédéric NOËL <contact@fredericnoel.com>
2020-01-28 12:15:42 +0100

    Test getter

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 
 require_once './classes/Voiture.php';
 
-$voiture1 = new Voiture();
+$voiture1 = new Voiture("Lada", "Niva", "blanc", [2000, 1500, 800], 900);
+$voiture2 = new Voiture("Nissan", "Cube", "violet", [2500, 1800, 1200], 1200);
 
-var_dump($voiture1);
+echo $voiture1->getMarque();
 
-$voiture1->couleur = "rouge";
-$voiture1->masse = 1000;
-$voiture1->modele = "R14";
-$voiture1->dimensions = [2000, 1500, 800];
-$dimensions = $voiture1->dimensions;
 
-$voiture1->afficherMessage();
+$voiture1->vitesse = 25;
+$voiture2->vitesse = 30;
+
+//echo $voiture1->calculerEnergieCinetique() . "<br />";
+//$voiture1->vitesse = 20;
+//echo $voiture1->calculerEnergieCinetique();
