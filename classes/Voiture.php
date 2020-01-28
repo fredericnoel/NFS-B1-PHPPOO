1a7c372b66f1c34cfaa23841773ac1842bc8bb85
Frédéric NOËL <contact@fredericnoel.com>
2020-01-28 11:37:50 +0100

    Classe Voiture
    - exemple de destructeur
    - exemple de constructeur

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -8,4 +8,15 @@ class Voiture
     public $dimensions = [0, 0, 0];
     public $masse;
     public $vitesse = 0;
+
+    public function __construct(string $mar, string $mod, string $cou, array $dim, int $mas)
+    {
+        $this->marque = $mar;
+        $this->modele = $mod;
+        $this->couleur = $cou;
+        $this->dimensions = $dim;
+        $this->masse = $mas;
+    }
+
+
 }
