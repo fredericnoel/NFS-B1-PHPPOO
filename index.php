76eb2a8560e1efb9c70ee5098e0680b272d231ed
Frédéric NOËL <contact@fredericnoel.com>
2020-01-28 14:22:22 +0100

    Test de la classe héritée Coupe

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once './classes/Vehicule.php';
 require_once './classes/Voiture.php';
+require_once './classes/Coupe.php';
+
+$toto = new Coupe("Peugeot", "504", "vert", [2000, 1000, 1000], 1200);
+var_dump($toto);
 
 $voiture1 = new Voiture("Lada", "Niva", "blanc", [2000, 1500, 800], 900);
 $voiture2 = new Voiture("Nissan", "Cube", "violet", [2500, 1800, 1200], 1200);
