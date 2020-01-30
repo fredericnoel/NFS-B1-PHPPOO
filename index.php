956f08de50635a74b245a20fe1d9561df4ca2080
Frédéric NOËL <contact@fredericnoel.com>
2020-01-30 12:21:03 +0100

    Appel méthodes pour affichage HTML

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
+$formulaire = new Form('index.php?page=validation', 'frmContact');
 
+$html = $formulaire->beginHtml('Je fais des formulaires en objet');
+$html .= $formulaire->displayForm();
+$html .= $formulaire->endHtml();
+echo $html;
