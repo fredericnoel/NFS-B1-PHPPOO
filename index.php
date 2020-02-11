b5c15674da22ad973523e4725e8b166041d39340
Frédéric NOËL <contact@fredericnoel.com>
2020-02-11 15:03:19 +0100

    Création tableau valeurs pour test Instanciation objet BDD Invocation méthode insérer

--- index.php
+++ index.php
@@ -3,11 +3,21 @@ date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
-Log::logWrite('Bonjour Michel');
+//Log::logWrite('Bonjour Michel');
 
-$formulaire = new Form('index.php?page=validation', 'frmContact');
+$trucEudRequete = array();
+array_push($trucEudRequete, ['NULL', '', '']);
+array_push($trucEudRequete, [':title', 'Je suis un message', 2]);
+array_push($trucEudRequete, [':content', 'Je suis un contenu', 2]);
+
+$query = new Bdd();
+$query->inserer('contact', $trucEudRequete);
+
+
+/*$formulaire = new Form('index.php?page=validation', 'frmContact');
 
 $html = $formulaire->beginHtml('Je fais des formulaires en objet');
 $html .= $formulaire->displayForm();
 $html .= $formulaire->endHtml();
 echo $html;
+*/
